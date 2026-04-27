6f19839b61740ba95b11e2ea1309575dd0344082
knight212121 <knight211221@gmail.com>
2026-04-27 12:52:56 +0530

    Cursor goes to the correct line after drawing title

--- src/output/output.c
+++ src/output/output.c
@@ -10,24 +10,23 @@ struct abuf {
     int len;
 };
 
-struct abuf ab = { NULL, 0 };
+struct abuf ab = {NULL, 0};
 
 void append_buffer(const char *s, int len) {
     char *new = realloc(ab.b, ab.len + len);
 
-    if (new == NULL) return;
+    if (new == NULL)
+        return;
     memcpy(&new[ab.len], s, len);
     ab.b = new;
     ab.len += len;
 }
 
-void free_buffer() {
-    free(ab.b);
-}
+void free_buffer() { free(ab.b); }
 
 void refresh_screen() {
     write(STDOUT_FILENO, "\x1b[2J", 4); // Remove all text from the screen
-    write(STDOUT_FILENO, "\x1b[H", 3); // Move cursor to the top
+    write(STDOUT_FILENO, "\x1b[H", 3);  // Move cursor to the top
 }
 
 void draw_title(const char *s) {
@@ -36,18 +35,23 @@ void draw_title(const char *s) {
     int welcomelen = snprintf(welcome, sizeof(welcome), "%s", s);
     int padding = (Term.screencols - welcomelen) / 2, i = 0;
 
-    while(padding) {
-        append_buffer(" ", 1);  // Draw spaces before the name
+    while (padding) {
+        append_buffer(" ", 1); // Draw spaces before the name
         padding--;
         i++;
     }
     append_buffer(s, strlen(s)); // Draw the name itself
-    i += strlen(s); // i is to draw spaces after the name
-    while(i < Term.screencols) {
-        append_buffer(" ", 1);  // Draw the spaces
+    i += strlen(s);              // i is to draw spaces after the name
+    while (i < Term.screencols) {
+        append_buffer(" ", 1); // Draw the spaces
         i++;
     }
     append_buffer("\x1b[0m", 4); // Return back to normal coloring
 
+    append_buffer("\x1b[2E",
+                  4); // Move the cursor two lines below at the beginning
+
     write(STDOUT_FILENO, ab.b, ab.len);
 }
+
+void draw_text_at_position(int rows, int cols) {}
